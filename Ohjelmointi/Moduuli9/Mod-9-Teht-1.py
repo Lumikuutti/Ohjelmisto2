a572e1a068094cd3474971eef62239d7cb5bde9f
Lumikuutti <227412132+lumikuutti@users.noreply.github.com>
2025-10-27 17:55:53 +0200

    new stuff

---
+++ Ohjelmointi/Moduuli9/Mod-9-Teht-1.py
@@ -0,0 +1,6 @@
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, nopeus = 0, kuljettumatka = 0):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = nopeus
+        self.kuljettumatka = kuljettumatka
