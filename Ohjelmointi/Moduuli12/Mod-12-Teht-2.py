7a24b29abb3804e51d9e340407c868163db07ac7
Lumikuutti <227412132+lumikuutti@users.noreply.github.com>
2025-11-04 17:49:34 +0200

    update

--- Ohjelmointi/Moduuli12/Mod-12-Teht-2.py
+++ Ohjelmointi/Moduuli12/Mod-12-Teht-2.py
@@ -1,10 +1,27 @@
 import requests
 
 API = "19f6d732c205e90611a8f5910cf174bd"
-call = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=" + API +"&units=metric"
+
+city_name = input("Give the name of your city: ")
+country_code = input("Give the country code: ")
+
+if country_code == "US":
+    state_code = input("Give the state code of your city: ")
+    coordinate_call = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API}"
+    coordinates = requests.get(coordinate_call).json()
+    longitude = (coordinates[0]['lon'])
+    latitude = (coordinates[0]['lat'])
+
+else:
+    coordinate_call = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&appid={API}"
+    coordinates = requests.get(coordinate_call).json()
+    longitude =(coordinates[0]['lon'])
+    latitude = (coordinates[0]['lat'])
+
+call = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API}&units=metric"
 response = requests.get(call).json()
 
 weather = (response["weather"][0]['main'])
 temperature = (response["main"]['temp'])
 
-print(f"{weather}{temperature}")
+print(f"Current weather in {city_name} is: {weather}\nCurrent temperature is: {temperature}")
