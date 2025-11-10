7f4c33b433411ff7d21eeb9b21b966ef03faaf2f
Lumikuutti <227412132+lumikuutti@users.noreply.github.com>
2025-11-10 22:28:34 +0200

    start of javascript

--- Ohjelmointi/Javascript/Moduuli1/Teht-7/Teht-7.js
+++ Ohjelmointi/Javascript/Moduuli1/Teht-7/Teht-7.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const rolls = parseInt(prompt('How many dice shall we roll?'));
-let result = 0
+let result = 0;
 
 for (let i = 1; i <= rolls; i += 1) {
-  let number  = Math.floor((Math.random() * 5) + 1);
-  result += number
+  let number = Math.floor((Math.random() * 5) + 1);
+  result += number;
 }
 
-console.log(`The sum of the dice is ${result}`)
+console.log(`The sum of the dice is ${result}`);
---
+++ Ohjelmointi/Javascript/Moduuli1/Teht-8/Teht-8.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const start = parseInt(prompt('Give the start year for the list.'));
+const end = parseInt(prompt('Give the end year for the list'));
+let year = start;
+
+while (year <= end) {
+  if ((year % 4) === 0 && (year % 100) !== 0) {
+    document.querySelector('#target').innerHTML += (year);
+    year += 1;
+  } else if ((year % 100) === 0 && (year % 400) === 0) {
+    document.querySelector('#target').innerHTML += (year);
+    year += 1;
+  }
+  else {
+    year += 1;
+  }
+  //document.querySelector('#target').innerHTML = (year);
+}
