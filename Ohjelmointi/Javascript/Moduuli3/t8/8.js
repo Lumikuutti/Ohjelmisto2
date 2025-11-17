0a7166b77a055eb20f62d25de738beddaff3ccd6
Lumikuutti <227412132+lumikuutti@users.noreply.github.com>
2025-11-18 00:53:35 +0200

    moduuli 3

---
+++ Ohjelmointi/Javascript/Moduuli3/t8/8.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const start = document.getElementById('start')
+
+start.addEventListener('click',function(evt){
+  evt.preventDefault()
+  let result = 0
+  const num1 = parseInt(document.getElementById('num1').value)
+  const num2  = parseInt(document.getElementById('num2').value)
+  const value = document.getElementById('operation').value
+
+  switch (value){
+    case 'add':
+      result = num1 + num2
+      document.querySelector('#result').innerHTML = (result)
+      break
+    case 'sub':
+      result = num1 - num2
+      document.querySelector('#result').innerHTML = (result)
+      break
+    case 'multi':
+      result = num1 * num2
+      document.querySelector('#result').innerHTML = (result)
+      break
+    case 'div':
+      result = num1 / num2
+      document.querySelector('#result').innerHTML = (result)
+      break
+  }
+})
